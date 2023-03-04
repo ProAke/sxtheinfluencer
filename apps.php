d4c391d10a2f6df571e999b260bfa0367248c729
Pro. 'Ake Worapot Bhilabutra <44732138+ProAke@users.noreply.github.com>
2023-03-04 21:38:00 +0700

    apps

---
+++ apps.php
@@ -0,0 +1,21 @@
+<?php error_reporting(E_ALL ^ E_NOTICE);
+/*****************************************************************
+Created :04/03/2565
+Author : worapot pilabut (aj.ake)
+E-mail : worapot.playdigital@gmail.com
+Website : https://www.playdigital.co.th
+Copyright (C) 2023, Play digital Co.,Ltd. all rights reserved.
+ *****************************************************************/
+
+
+include_once("../include/config.inc.php");
+include_once("../include/function.inc.php");
+
+
+
+
+echo "Welcome to Play Digital Co.,Ltd.";
+
+
+
+?>
