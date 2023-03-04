95d913d5585860a50f69e05d9ba27fff8e2cff8a
Pro. 'Ake Worapot Bhilabutra <44732138+ProAke@users.noreply.github.com>
2023-03-04 22:02:03 +0700

    api

--- apps.php
+++ apps.php
@@ -1,21 +1,78 @@
-<?php error_reporting(E_ALL ^ E_NOTICE);
-/*****************************************************************
-Created :04/03/2565
-Author : worapot pilabut (aj.ake)
-E-mail : worapot.playdigital@gmail.com
-Website : https://www.playdigital.co.th
-Copyright (C) 2023, Play digital Co.,Ltd. all rights reserved.
- *****************************************************************/
+<?php
+include 'DbConnect.php';
+$objDb = new DbConnect;
+$conn = $objDb->connect();
 
+$method = $_SERVER['REQUEST_METHOD'];
+switch($method) {
+    case "GET":
+        $sql = "SELECT * FROM tb_app_form";
+        $path = explode('/', $_SERVER['REQUEST_URI']);
+        if(isset($path[3]) && is_numeric($path[3])) {
+            $sql .= " WHERE id = :id";
+            $stmt = $conn->prepare($sql);
+            $stmt->bindParam(':id', $path[3]);
+            $stmt->execute();
+            $users = $stmt->fetch(PDO::FETCH_ASSOC);
+        } else {
+            $stmt = $conn->prepare($sql);
+            $stmt->execute();
+            $users = $stmt->fetchAll(PDO::FETCH_ASSOC);
+        }
 
-include_once("../include/config.inc.php");
-include_once("../include/function.inc.php");
+        echo json_encode($users);
+        break;
+    case "POST":
+        $user = json_decode( file_get_contents('php://input') );
+        $sql = "INSERT INTO tb_app_form(id, name, email, mobile, created_at) VALUES(null, :name, :email, :mobile, :created_at)";
+        $stmt = $conn->prepare($sql);
+        $created_at = date('Y-m-d');
+        $stmt->bindParam(':name', $user->name);
+        $stmt->bindParam(':email', $user->email);
+        $stmt->bindParam(':mobile', $user->mobile);
+        $stmt->bindParam(':created_at', $created_at);
 
+        if($stmt->execute()) {
+            $response = ['status' => 1, 'message' => 'Record created successfully.'];
+        } else {
+            $response = ['status' => 0, 'message' => 'Failed to create record.'];
+        }
+        echo json_encode($response);
+        break;
 
+    case "PUT":
+        $user = json_decode( file_get_contents('php://input') );
+        $sql = "UPDATE tb_app_form SET name= :name, email =:email, mobile =:mobile, updated_at =:updated_at WHERE id = :id";
+        $stmt = $conn->prepare($sql);
+        $updated_at = date('Y-m-d');
+        $stmt->bindParam(':id', $user->id);
+        $stmt->bindParam(':name', $user->name);
+        $stmt->bindParam(':email', $user->email);
+        $stmt->bindParam(':mobile', $user->mobile);
+        $stmt->bindParam(':updated_at', $updated_at);
 
+        if($stmt->execute()) {
+            $response = ['status' => 1, 'message' => 'Record updated successfully.'];
+        } else {
+            $response = ['status' => 0, 'message' => 'Failed to update record.'];
+        }
+        echo json_encode($response);
+        break;
 
-echo "Welcome to Play Digital Co.,Ltd.";
+    case "DELETE":
+        $sql = "DELETE FROM tb_app_form WHERE id = :id";
+        $path = explode('/', $_SERVER['REQUEST_URI']);
 
+        $stmt = $conn->prepare($sql);
+        $stmt->bindParam(':id', $path[3]);
 
+        if($stmt->execute()) {
+            $response = ['status' => 1, 'message' => 'Record deleted successfully.'];
+        } else {
+            $response = ['status' => 0, 'message' => 'Failed to delete record.'];
+        }
+        echo json_encode($response);
+        break;
+}
 
 ?>
