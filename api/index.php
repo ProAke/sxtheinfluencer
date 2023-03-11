be2903d511b5bfe70cb263279a486253e26a8b93
Pro. 'Ake Worapot Bhilabutra <44732138+ProAke@users.noreply.github.com>
2023-03-11 21:02:49 +0700

    index3

--- api/index.php
+++ api/index.php
@@ -1,6 +1,6 @@
 <?php
-//้header("Access-Control-Allow-Origin: *");
-//header("Content-Type: application/json; charset=UTF-8");
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json; charset=UTF-8");
 
 $mydata = '';
 foreach($_REQUEST as $key => $value) {
