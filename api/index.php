9110ea630e3e08ca83a7c9a5f50d2b5aa558047c
Pro. 'Ake Worapot Bhilabutra <44732138+ProAke@users.noreply.github.com>
2023-03-11 22:25:01 +0700

    db TeamName

--- api/index.php
+++ api/index.php
@@ -6,13 +6,15 @@ header("Content-Type: application/json; charset=UTF-8");
 $mydata = '';
 foreach($_REQUEST as $key => $value) {
     if(!empty($value)) {
-        $mydata = $mydata.$key . ' : ' . $value . '\n';
+        $mydata .= $key . ' : ' . $value . '\n';
+        if($key=='TeamName'){ $TeamName = $value; }
     }
 
 }
 
 $myfile = fopen("db.txt", "w") or die("Unable to open file!");
 fwrite($myfile, $mydata);
+fwrite($myfile, $TeamName);
 fclose($myfile);
 
 echo "200";
