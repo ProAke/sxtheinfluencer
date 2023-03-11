f8354953e882c369c3f588d417d23b4f84c0f48a
Pro. 'Ake Worapot Bhilabutra <44732138+ProAke@users.noreply.github.com>
2023-03-11 23:01:23 +0700

    value data

--- api/index.php
+++ api/index.php
@@ -16,11 +16,11 @@ foreach($_REQUEST as $key => $value) {
 
 */
 
-$data ='entries : {"TeamName":"abc","ProjectName":"WowWow","T1FName":"wr","T1LName":"fc","T1NickName":"f","T1Bdate":"11-03-2023","T1Phone":"0","T1FB":"\u0e48https:\/\/facebook.com\/ake.pro","T1Email":"","T1JobDescription":"do you ","agreement3":"Agreement3"}entry_id : 856form_id : 822version : 3.2.3file_uploads : {"T1CardUpload":[{"url":"https:\/\/www.sxtheinfluencer.com\/wp-content\/uploads\/2023\/03\/1678549156-sr9WySZCLW.png","error":false,"name":"sr9WySZCLW.png","file":"\/var\/www\/vhosts\/sxtheinfluencer.com\/httpdocs\/wp-content\/uploads\/2023\/03\/1678549156-sr9WySZCLW.png","type":"image\/png"}]}
-';
+$data ='{entries : {"TeamName":"abc","ProjectName":"WowWow","T1FName":"wr","T1LName":"fc","T1NickName":"f","T1Bdate":"11-03-2023","T1Phone":"0","T1FB":"\u0e48https:\/\/facebook.com\/ake.pro","T1Email":"","T1JobDescription":"do you ","agreement3":"Agreement3"}entry_id : 856form_id : 822version : 3.2.3file_uploads : {"T1CardUpload":[{"url":"https:\/\/www.sxtheinfluencer.com\/wp-content\/uploads\/2023\/03\/1678549156-sr9WySZCLW.png","error":false,"name":"sr9WySZCLW.png","file":"\/var\/www\/vhosts\/sxtheinfluencer.com\/httpdocs\/wp-content\/uploads\/2023\/03\/1678549156-sr9WySZCLW.png","type":"image\/png"}]}
+}';
 
-$TeamName = explode('TeamName : ', $data);
-$ProjectName = explode('ProjectName : ', $data);
+$TeamName = json_decode($data)->entries->TeamName;
+$ProjectName = json_decode($data)->entries->ProjectName;
 
 
 
