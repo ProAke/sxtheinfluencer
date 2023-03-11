ddb09129ee74d25f07eb0b3e9688b105369ad6c4
Pro. 'Ake Worapot Bhilabutra <44732138+ProAke@users.noreply.github.com>
2023-03-11 20:59:08 +0700

    index.php update

--- api/index.php
+++ api/index.php
@@ -1,7 +1,5 @@
 <?php
 ้header("Access-Control-Allow-Origin: *");
-header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
-header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
 header("Content-Type: application/json; charset=UTF-8");
 
 $mydata = '';
@@ -15,98 +13,4 @@ $myfile = fopen("db.txt", "w") or die("Unable to open file!");
 fwrite($myfile, $mydata);
 fclose($myfile);
 
-echo "200";
-
-
-/*
-
-include 'DbConnect.php';
-$objDb = new DbConnect;
-$conn = $objDb->connect();
-
-$method = $_SERVER['REQUEST_METHOD'];
-switch($method) {
-    case "GET":
-        $sql = "SELECT * FROM tb_app_form";
-        $path = explode('/', $_SERVER['REQUEST_URI']);
-        if(isset($path[3]) && is_numeric($path[3])) {
-            $sql .= " WHERE id = :id";
-            $stmt = $conn->prepare($sql);
-            $stmt->bindParam(':id', $path[3]);
-            $stmt->execute();
-            $users = $stmt->fetch(PDO::FETCH_ASSOC);
-        } else {
-            $stmt = $conn->prepare($sql);
-            $stmt->execute();
-            $users = $stmt->fetchAll(PDO::FETCH_ASSOC);
-        }
-
-        echo json_encode($users);
-        break;
-    case "POST":
-        $user = json_decode( file_get_contents('php://input') );
-        $sql = "INSERT INTO tb_app_form(id, TeamName, created_at) VALUES(null, :TeamName, :created_at)";
-        $stmt = $conn->prepare($sql);
-        $created_at = date('Y-m-d');
-        $stmt->bindParam(':TeamName', $user->TeamName);
-        $stmt->bindParam(':created_at', $created_at);        
-       // $sql = "INSERT INTO tb_app_form(id, name, email, mobile, created_at) VALUES(null, :name, :email, :mobile, :created_at)";
-
-
-
-
-        
-//       $stmt->bindParam(':name', $user->name);
-//       $stmt->bindParam(':email', $user->email);
-//       $stmt->bindParam(':mobile', $user->mobile);
-//       $stmt->bindParam(':created_at', $created_at);
-
-
-/*
-
-        if($stmt->execute()) {
-            $response = ['status' => 1, 'message' => 'Record created successfully.'];
-        } else {
-            $response = ['status' => 0, 'message' => 'Failed to create record.'];
-        }
-        echo json_encode($response);
-        break;
-
-    case "PUT":
-        $user = json_decode( file_get_contents('php://input') );
-        $sql = "UPDATE tb_app_form SET name= :name, email =:email, mobile =:mobile, updated_at =:updated_at WHERE id = :id";
-        $stmt = $conn->prepare($sql);
-        $updated_at = date('Y-m-d');
-        $stmt->bindParam(':id', $user->id);
-        $stmt->bindParam(':name', $user->name);
-        $stmt->bindParam(':email', $user->email);
-        $stmt->bindParam(':mobile', $user->mobile);
-        $stmt->bindParam(':updated_at', $updated_at);
-
-        if($stmt->execute()) {
-            $response = ['status' => 1, 'message' => 'Record updated successfully.'];
-        } else {
-            $response = ['status' => 0, 'message' => 'Failed to update record.'];
-        }
-        echo json_encode($response);
-        break;
-
-    case "DELETE":
-        $sql = "DELETE FROM tb_app_form WHERE id = :id";
-        $path = explode('/', $_SERVER['REQUEST_URI']);
-
-        $stmt = $conn->prepare($sql);
-        $stmt->bindParam(':id', $path[3]);
-
-        if($stmt->execute()) {
-            $response = ['status' => 1, 'message' => 'Record deleted successfully.'];
-        } else {
-            $response = ['status' => 0, 'message' => 'Failed to delete record.'];
-        }
-        echo json_encode($response);
-        break;
-
-        
-}
-*/
 ?>
