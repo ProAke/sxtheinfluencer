fc62bb0d21bad01c8ec99079195a998df14dce4b
Pro. 'Ake Worapot Bhilabutra <44732138+ProAke@users.noreply.github.com>
2023-03-11 21:18:23 +0700

    data

--- api/index.php
+++ api/index.php
@@ -2,15 +2,34 @@
 header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json; charset=UTF-8");
 
-$mydata = '';
-foreach($_REQUEST as $key => $value) {
-    if(!empty($value)) {
-        $mydata = $mydata.$key . ' : ' . $value . '\n';
-    }
-}
+
+$data = json_decode($_REQUEST['entries'], true);
+$TeamName = $data['TeamName'];
+$ProjectName = $data['ProjectName'];
+$T1FName = $data['T1FName'];
+$T1LName = $data['T1LName'];
+$T1NickName = $data['T1NickName'];
+$T1Bdate = $data['T1Bdate'];
+$T1Phone = $data['T1Phone'];
+$T1FB = $data['T1FB'];
+$T1Email = $data['T1Email'];
+$T1JobDescription = $data['T1JobDescription'];
+$agreement3 = $data['agreement3'];
+$T1CardUpload = $data['file_uploads']['T1CardUpload'][0]['url'];
 
 $myfile = fopen("db.txt", "w") or die("Unable to open file!");
-fwrite($myfile, $mydata);
+fwrite($myfile, $TeamName);
+fwrite($myfile, $ProjectName);
 fclose($myfile);
+
+
+
+
+
+
+
+
+
+
 
 ?>
