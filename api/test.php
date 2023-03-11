47d85e006d5fbd69bedf48d4c03764998d5f7730
Pro. 'Ake Worapot Bhilabutra <44732138+ProAke@users.noreply.github.com>
2023-03-11 17:42:34 +0700

    test.php

---
+++ api/test.php
@@ -0,0 +1,18 @@
+<?php
+้header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json; charset=UTF-8");
+
+$mydata = '';
+foreach($_REQUEST as $key => $value) {
+    if(!empty($value)) {
+        $mydata .= $key . ' : ' . $value . '\n';
+    }
+}
+
+$myfile = fopen("db.txt", "w") or die("Unable to open file!");
+fwrite($myfile, $mydata);
+fclose($myfile);
+
+echo "200";
+
+?>
