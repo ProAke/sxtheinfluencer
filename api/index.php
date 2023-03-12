f5f60318a5ef57625b27394d444ea9d3a86c6da2
Pro. 'Ake Worapot Bhilabutra <44732138+ProAke@users.noreply.github.com>
2023-03-12 20:10:27 +0700

    rs

--- api/index.php
+++ api/index.php
@@ -11,7 +11,7 @@ include_once("../include/function.inc.php");
 
 
 $data = $_REQUEST;
-$TeamName = $_REQUEST[0]['TeamName'].$_REQUEST[1]['TeamName'].$_REQUEST[2]['TeamName'];
+$TeamName = "-->".$_REQUEST[3];
 $created_at = date("Y-m-d H:i:s");
 
 
